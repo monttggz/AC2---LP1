dba256b077a9ef7a3a5f7813e35bf0415b83176d
monttggz <rm16248@fiap.com.br>
2024-10-28 08:29:43 -0300

    aaaaaaaa
    aaaaaaaaaaaa

--- Ex2/script.js
+++ Ex2/script.js
@@ -1,15 +1,17 @@
 let numeros = [2, 5, 8, 13, 16, 21]
+const multiplicador = Math.floor(Math.random() * 10)
 
 let numeros_impares = numeros.filter(function(num){
+    if (num%2 == 1) {
+        return num * multiplicador
+    }
+})
+let numeros_pares = numeros.filter(function(num){
     if (num%2 == 0) {
         return num
     }
 })
 
-console.log(numeros_impares)
+let numeros_novo = numeros_pares.concat(numeros_impares)
 
-numeros
-
-if (num%2 == 0) {
-
-}
+console.log(numeros_novo)
---
+++ Ex3/script.js
@@ -0,0 +1,3 @@
+let numeros = [1.2, 3.7, 4.5, 6.1, 8.9]
+
+numeros.forEach
