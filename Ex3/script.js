dd2a59bafaf329f097084e7da428adcba18cc38b
monttggz <rm16248@fiap.com.br>
2024-10-28 08:36:26 -0300

    pronto 3
    mitei

--- Ex3/script.js
+++ Ex3/script.js
@@ -1,8 +1,10 @@
 let numeros = [1.2, 3.7, 4.5, 6.1, 8.9]
 let novos_numeros = []
+
 function arredondar(item){
     let arrendondado = Math.ceil(item)
     novos_numeros.push(arrendondado)
 }
+
 numeros.forEach(arredondar)
 console.log(numeros)
