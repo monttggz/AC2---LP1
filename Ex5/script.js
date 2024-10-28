6d14ba831265bd70643457090b95aba72979112f
monttggz <rm16248@fiap.com.br>
2024-10-28 08:46:06 -0300

    aaaaaaaaaaaaaaaaaaaa
    terminei

--- Ex5/script.js
+++ Ex5/script.js
@@ -1,3 +1,8 @@
 let novosNumeros = [1, 2, 3]
+let numerosOriginais = [4, 5, 6]
 
-let numeros_novo = numeros_pares.concat(numeros_impares)
+let numeros_novo = novosNumeros.concat(numerosOriginais)
+let numerosNovo = numerosNovo.push(novosNumeros) && numerosOriginais.push(numerosOriginais)
+
+console.log(numeros_novo)
+console.log(numerosNovo)
