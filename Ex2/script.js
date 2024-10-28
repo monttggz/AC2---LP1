a7d37d63148b921c3cb667075cb02c77cb7ef444
monttggz <rm16248@fiap.com.br>
2024-10-28 08:13:07 -0300

    parte feito

--- Ex2/script.js
+++ Ex2/script.js
@@ -1,5 +1,13 @@
 let numeros = [2, 5, 8, 13, 16, 21]
 
+let numeros_impares = numeros.filter(function(num){
+    if (num%2 == 0) {
+        return num
+    }
+})
+
+console.log(numeros_impares)
+
 numeros
 
 if (num%2 == 0) {
