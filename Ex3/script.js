77c021bad2b7b1dbce52a818906eeb79d54ad29a
monttggz <rm16248@fiap.com.br>
2024-10-28 08:32:17 -0300

    Update script.js

--- Ex3/script.js
+++ Ex3/script.js
@@ -1,3 +1,9 @@
 let numeros = [1.2, 3.7, 4.5, 6.1, 8.9]
+function arredondar(item){
+    let arrendondado = Math.round(item)
+    return arrendondado
+}
 
-numeros.forEach
+numeros.forEach(arredondar)
+
+console.log(numeros)
